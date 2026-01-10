0b6ee60b668017347854d48350e3444483695380
3vax <Hauk@Spurvehauk.nl>
2026-01-10 13:55:49 +0100

    Fixed a small issue with delimiter

--- DhcpLeasesToCsv.ps1
+++ DhcpLeasesToCsv.ps1
@@ -1,4 +1,4 @@
 # This script retrieves DHCP leases from a specified DHCP server and exports them to a CSV file with a custom delimiter.
 Get-DhcpServerv4Scope -ComputerName $dhcpServer | ForEach-Object {
     Get-DhcpServerv4Lease -ComputerName $dhcpServer -ScopeId $_.ScopeId -AllLeases
-} | Select-Object ScopeId, IPAddress, HostName, ClientId, AddressState | Export-Csv -Path "DhcpLeases.csv" -Delimiter "::" -NoTypeInformation
+} | Select-Object ScopeId, IPAddress, HostName, ClientId, AddressState | Export-Csv -Path "DhcpLeases.csv" -Delimiter ":" -NoTypeInformation
